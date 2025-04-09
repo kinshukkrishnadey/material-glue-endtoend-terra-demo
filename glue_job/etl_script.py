d3f537e53fb45e9890d0e357c91abff1cbdab658
Kinshuk Dey <72959893+kinshukkrishnadey@users.noreply.github.com>
2025-04-10 05:07:29 +0530

    Update etl_script.py

--- glue_job/etl_script.py
+++ glue_job/etl_script.py
@@ -5,26 +5,39 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+# Glue boilerplate
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
+job.init(args["JOB_NAME"], args)
 
-# Read from catalog
+# Read from PostgreSQL using Glue Data Catalog
 datasource = glueContext.create_dynamic_frame.from_catalog(
-    database="postgres_catalog",
-    table_name="postgres_table"
+    database = "postgres_catalog",
+    table_name = "customers"  # Must match catalog name
 )
 
-# Write to S3 as Parquet
+# Optional: Apply mapping / transformation
+mapped = ApplyMapping.apply(
+    frame = datasource,
+    mappings = [
+        ("customerid", "int", "customerid", "int"),
+        ("first_name", "string", "first_name", "string"),
+        ("last_name", "string", "last_name", "string"),
+        ("company", "string", "company", "string"),
+        ("country", "string", "country", "string"),
+        ("email", "string", "email", "string"),
+    ]
+)
+
+# Save to S3 in Parquet
 glueContext.write_dynamic_frame.from_options(
-    frame=datasource,
-    connection_type="s3",
-    connection_options={"path": "s3://my-glue-etl-bucket-demo/output/"},
-    format="parquet"
+    frame = mapped,
+    connection_type = "s3",
+    connection_options = {"path": "s3://your-bucket/output/"},
+    format = "parquet"
 )
 
 job.commit()
-
