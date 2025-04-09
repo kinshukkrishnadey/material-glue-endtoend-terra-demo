dd0f32ead36b659298cc418d2d18cb57474fad9d
Kinshuk Dey <72959893+kinshukkrishnadey@users.noreply.github.com>
2025-04-09 20:19:03 +0530

    Update etl_script.py

--- glue_job/etl_script.py
+++ glue_job/etl_script.py
@@ -1,1 +1,30 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Read from catalog
+datasource = glueContext.create_dynamic_frame.from_catalog(
+    database="postgres_catalog",
+    table_name="postgres_table"
+)
+
+# Write to S3 as Parquet
+glueContext.write_dynamic_frame.from_options(
+    frame=datasource,
+    connection_type="s3",
+    connection_options={"path": "s3://my-glue-etl-bucket-demo/output/"},
+    format="parquet"
+)
+
+job.commit()
 
