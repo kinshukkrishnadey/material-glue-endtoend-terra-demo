55ef3e648fe29ef1dbcde3a422955086501b9a2f
Kinshuk Dey <72959893+kinshukkrishnadey@users.noreply.github.com>
2025-04-09 20:15:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "s3_bucket" {
+  default = "my-glue-etl-bucket-demo"
+}
+
+variable "glue_catalog_database" {
+  default = "postgres_catalog"
+}
+
+variable "glue_catalog_table" {
+  default = "postgres_table"
+}
+
+variable "glue_job_name" {
+  default = "postgres-to-parquet-etl"
+}
 
