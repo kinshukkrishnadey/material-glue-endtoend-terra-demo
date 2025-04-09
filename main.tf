a6accc1bd1161f2f1a2b7b48609107414123b75e
Kinshuk Dey <72959893+kinshukkrishnadey@users.noreply.github.com>
2025-04-10 03:10:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,25 +14,38 @@ resource "aws_s3_object" "glue_script" {
   etag   = filemd5("${path.module}/glue_job/etl_script.py")
 }
 
+# Glue IAM Role
 resource "aws_iam_role" "glue_service_role" {
   name = "glue-service-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
+      Effect = "Allow"
       Principal = {
         Service = "glue.amazonaws.com"
       }
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "glue_policy" {
+# Attach AWS managed policies to Glue role
+resource "aws_iam_role_policy_attachment" "glue_service_policy" {
   role       = aws_iam_role.glue_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
+
+resource "aws_iam_role_policy_attachment" "glue_console_access" {
+  role       = aws_iam_role.glue_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.glue_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
 
 resource "aws_glue_catalog_database" "catalog_db" {
   name = var.glue_catalog_database
